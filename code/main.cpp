11c549c44b5b8b5ecac9ef374d6ab0021e8f762a
firenebula <brownjoh@onid.oregonstate.edu>
2017-01-25 00:39:49 -0600

    adding Parser Object

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include "Parser.h"
+void testParseVal(string, Parser);
 
-//using namespace std;
 using std::cout;
 using std::endl;
 
 int main()
 {
-    cout << "Hello Hercules!" << endl;
+    Parser hParser;  // new Parser Object
+    testParseVal("Default Values: ", hParser); // default values set to -1
+    hParser.parse("any input"); // will parse input.  For now, just updating to dummy values
+    testParseVal("After Parse: ", hParser);
     return 0;
+}
+
+
+// This test function is just printing out the contents of the Parse obj variables
+void testParseVal(string label, Parser p){
+
+    cout << label << endl << "Action Value: " << p.getAction() << endl
+    << "Object Value: " << p.getObject() << endl
+    << "Indirect Value: " << p.getIndirect() << endl;
+
+
+
 }
