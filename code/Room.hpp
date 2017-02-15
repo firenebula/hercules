202d617e9716cebce6ca4754f46f39d08a4c1b93
Phuc Vo <voph@flip3.engr.oregonstate.edu>
2017-02-14 16:09:06 -0800

    Expanded number of exits to 6

--- code/Room.hpp
+++ code/Room.hpp
@@ -20,7 +20,7 @@ using std::cin;
 using std::endl;
 using std::string;
 
-const int NUM_OF_EXITS = 4;
+const int NUM_OF_EXITS = 6;
 
 class Room
 {
@@ -29,8 +29,10 @@ class Room
       string desc_short;
       string desc_long;
       bool visited;
-      std::string exits[NUM_OF_EXITS];
+      std::string exits[NUM_OF_EXITS];  // index 0 = north, 1 = south, 2 = east, 3 = west, 4 = up, 5 = down
 
+	  
+	  
    public:
 	Room(std::string name);
 	void setDescLong(std::string desc_long);
